4598b798e268d9bcc31d837e4373698f9ea48ad7
Maciej Maziuk <maciej@maziuk.com.pl>
2021-10-31 19:45:53 +0000

    Update ZAD7.py POPRAWNE !!!

--- lab4/ZAD7.py
+++ lab4/ZAD7.py
@@ -1,24 +1,19 @@
-lista1=[-1, 2,  3 ,4]
-y = lista1
+lista1=[-1, 2,  3, 6]
+OldList = list(lista1)
 currentnum = 0
 bignum = lista1[0]
 x = 0
 for i in range(len(lista1)-1):
     currentnum = lista1[i+1]
-    if bignum > currentnum:
+    if bignum < currentnum:
         bignum = currentnum
-        x = lista1.index(bignum)
 
-
-lista1 = lista1[0:x]+ lista1[x+1:]
-
-for i in range(len(lista1) - 1):
+lista1.remove(bignum)
+for i in range(len(lista1)-1):
     bignum=lista1[0]
     currentnum = lista1[i+1]
     if bignum < currentnum:
         bignum = currentnum
-        x = lista1.index(bignum)
+        x = OldList.index(bignum)
 
 print("Element z listy index: ", x, "i ten numer: ", bignum)
-
-#DOKONCZ W DOMU!!!!!!!
