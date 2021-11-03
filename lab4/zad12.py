586f68c85ad7b164bfe2c76e8ae140dd695ba898
Maciej Maziuk <maciej@maziuk.com.pl>
2021-11-03 02:20:28 +0000

    Update zad12.py

--- lab4/zad12.py
+++ lab4/zad12.py
@@ -1,26 +1,11 @@
-word = "rabarbar"
-wzorzec = "ra"
-n = 0
-print(word[0:2])
-box = ""
-i = 0
-while 0 < len(word)-i:
-
-
-
-    box.append(word[i])
-    if len(box) == len(wzorzec) and box == word[n:i+1]:
-        print("Jest wzorzec")
-        n+=1
-        i+=1
-    elif len(box) == len(wzorzec):
-        n+=1
-        box = ""
-        i+=1
-    else:
-        n+=1
-        i+=1
-
+slowo="rabarbar"
+wzorzec="ab"
+help=0
+for i in range (0,len(slowo)):
+    if slowo[i:len(wzorzec)+i]==wzorzec:
+        help=1
+if help==1:
+    print("tak")
 
 
 
