1c7ddfafeb7021d135ed3f29d65522749822f168
Maciej Maziuk <maciej@maziuk.com.pl>
2021-11-14 22:14:15 +0000

    Update zad9.py

--- lab5/zad9.py
+++ lab5/zad9.py
@@ -1,9 +1,7 @@
-#napisz program ktory z listy list wybierze liste o najmniejszej
-# i najwiekszej sumie xd laura jest glupia i patrzy mi w monitor
 WholeList = []
-list1 =  [1,2,3,4,100]
+list1 =  [1,2,3]
 list2 = [1,1,1,1,1,20]
-list3 = [5,4,6]
+list3 = [5,4,600]
 
 WholeList.append(list2)
 WholeList.append(list3)
@@ -22,16 +20,20 @@ for i in range(len(WholeList)):
 print("najwieksza suma:", BigSum)
 
 #NAJMNIEJSZA SUMA W LISCIE :
-SmallSum= 0
+SmallSum=0
+for j in range(len(WholeList[0])):
+    SmallSum += (WholeList[0])[j]
+
+
+
 for i in range(len(WholeList)):
     Sum = 0
     for j in range(len(WholeList[i])):
         Sum += (WholeList[i])[j]
-    print(Sum)
-# do tego momentu jest dobrze !!!!
-    if Sum < BigSum:
+
+    if Sum < SmallSum:
         SmallSum = Sum
-        if Sum < SmallSum:
-            SmallSum = Sum
+
 
 print("najmniejsza suma:", SmallSum)
+
