0acb22f31988416ab2caa76760436a6b9ae1b53f
Maciej Maziuk <maciej@maziuk.com.pl>
2021-11-14 20:56:09 +0000

    Upload New File

---
+++ lab5/zad11.py
@@ -0,0 +1,15 @@
+list = [100,2,3,40,5,6]
+bignum = list[0]
+
+for i in range(len(list)):
+    print(i)
+    if list[i] > bignum:
+        bignum = list[i]
+
+        x = i
+
+
+print(bignum)
+print(list[a])
+
+# wypisz drugi element z listy, sprawdz jak kopiowac zmienna zeby byla syala
