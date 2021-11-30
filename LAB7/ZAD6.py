3879cb7727edf5bc2b424599b4db423793498425
Maciej Maziuk <maciej@maziuk.com.pl>
2021-11-30 17:07:37 +0000

    Upload New File

---
+++ LAB7/ZAD6.py
@@ -0,0 +1,16 @@
+def check(list, x):
+    ListOfX = []
+    ListofBigger = []
+    ListofSmaller = []
+    n = len(list)-1
+    while n >= 0:
+        if list[n] == x:
+            ListOfX.append(list[n])
+        elif list[n] < x:
+            ListofSmaller.append(list[n])
+        else:
+            ListofBigger.append(list[n])
+        n -=1
+    return len(ListOfX), len(ListofSmaller), len(ListofBigger)
+y = check([1,2,3,4,4,4,4,5,6,7,8], 4)
+print("Result: ", y)
