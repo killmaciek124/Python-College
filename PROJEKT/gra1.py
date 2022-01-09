91e8e7144cb253e0f2a439f67da2e74d3cf31f09
Maciej Maziuk <maciej@maziuk.com.pl>
2022-01-09 22:54:12 +0000

    Update PROJEKT/gra1.py

--- PROJEKT/gra1.py
+++ PROJEKT/gra1.py
@@ -1,7 +1,34 @@
-# link do tworzenia tabeli (szachownicy) https://ichi.pro/pl/jak-latwo-tworzyc-tabele-w-pythonie-51327615359229
 from tabulate import tabulate
-table = [['First', 'Last', 'Age'], ['John', 'Smith', 39], ['Mary', 'Jane', 25], ['Jenny', 'Doe', 28]]
-#print(tabulate(table, headers='firstrow', tablefmt='fancy_grid'))
-#def itemplacement(table):
-info = {'First Name': ['John', 'Mary', 'Jennifer'], 'Last Name': ['Smith', 'Jane', 'Doe'], 'Age': [39, 25, 28]}
-print(tabulate(info, headers='keys', tablefmt='fancy_grid',  ))
+from numpy import random
+from time import sleep
+
+info = [['', '1', '2', '3', '4'], ['A', '', '', '',''], ['B', '', '', '',''], ['C', '', '', '',''],
+            ['D', '', '', '','']]
+# WSZYSTKIE PODFUNKCJE :
+def randomsign():
+
+    x = random.randint(1,5)
+    y = random.randint(1,5) # random cyfra do indeksu litery
+    info[x][y] = 'X'
+
+#def checkguess(column, row):
+
+
+
+
+
+
+
+# FUNKCJA MAIN !!!!!
+def main():
+
+    randomsign()
+    print("Witaj graczu!\nMusisz znaleźć ukryty 'X' podając koordynaty pól.\n"
+          "Jeśli trafisz wygrywasz, jeśli nie, powiem ci, jak daleko jest ukryty znak.\n"
+          "Poniżej jest podana tabela i jej oznaczenia. Aby zgadywać, wpisz znak kolumny i wiersza,\n"
+          "np. b3\nPOWODZENIA!")
+    print(tabulate(info, tablefmt='fancy_grid',))
+    guesscolumn = input("Podaj numer kolumny: ")
+    guessrow = input("Podaj numer wiersza: ")
+
+main()
